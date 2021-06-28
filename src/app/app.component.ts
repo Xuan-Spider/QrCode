4b81c76d05e2683e5f44d91c3eedbbc9203978e6
Xuan-Spider <dinhxuan9807@gmail.com>
2021-06-29 06:48:13 +0700

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
+import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'qrcode-demo';
+export class AppComponent   {
+  title = 'qr-code-test';
+  imgURL = "../../assets/recruit.svg";
+  textValue = "";
+  @ViewChild('qrCode', {static : false}) qrCode:any;
+
+  createQRWithImage(){
+    
+    let elem =  this.qrCode.elementRef.nativeElement.children[0];
+    let context = elem.getContext("2d");
+
+    let img = new Image();
+    img.src = this.imgURL;
+    let iWidth = 50;
+    let iHeight = 50;
+    let me = this; 
+    img.onload = () => {
+      context.drawImage(img, (elem.width/2) - (iWidth/2),(elem.height/2) - (iHeight/2), iWidth, iHeight);    
+      saveAs(me.canvasToBlob(elem), "file.png");
+    }
+    this.qrCode.elementRef.nativeElement.download
+  }
+
+  canvasToBlob(canvas){
+    let dataurl = canvas.toDataURL("image/png");
+    let byteString = window.atob(dataurl.replace(/^data:image\/(png|jpg);base64,/, ""));
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([int8Array], { type: 'image/jpeg' });
+  }
 }

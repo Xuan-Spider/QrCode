bda719b2e979e547ecb5d22bc31944a97383791a
Xuan-Spider <dinhxuan9807@gmail.com>
2021-07-02 01:03:26 +0700

    add few fn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ViewChild } from '@angular/core';
+import { AfterViewInit, OnInit, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
 import { saveAs } from 'file-saver';
 
@@ -7,30 +7,39 @@ import { saveAs } from 'file-saver';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent   {
-  title = 'qr-code-test';
+export class AppComponent implements AfterViewInit {
   imgURL = "../../assets/recruit.svg";
   textValue = "";
-  @ViewChild('qrCode', {static : false}) qrCode:any;
+  @ViewChild('qrCode', { static: false }) qrCode: any;
 
-  createQRWithImage(){
-    
-    let elem =  this.qrCode.elementRef.nativeElement.children[0];
-    let context = elem.getContext("2d");
-
-    let img = new Image();
-    img.src = this.imgURL;
-    let iWidth = 50;
-    let iHeight = 50;
-    let me = this; 
-    img.onload = () => {
-      context.drawImage(img, (elem.width/2) - (iWidth/2),(elem.height/2) - (iHeight/2), iWidth, iHeight);    
-      saveAs(me.canvasToBlob(elem), "file.png");
-    }
-    this.qrCode.elementRef.nativeElement.download
+  ngAfterViewInit() {
+    this.createQRWithImage();
   }
 
-  canvasToBlob(canvas){
+  createQRWithImage() {
+    let canvas = this.qrCode.elementRef.nativeElement.children[0];
+    let context = canvas.getContext("2d");
+    let img = new Image();
+    img.src = this.imgURL;
+    let iWidth = 30;
+    let iHeight = 30;
+    img.onload = () => {
+      context.drawImage(img, (canvas.width / 2) - (iWidth / 2), (canvas.height / 2) - (iHeight / 2), iWidth, iHeight);
+    }
+  }
+
+  
+  saveQRCode() {
+    let elem = this.qrCode.elementRef.nativeElement.children[0];
+    saveAs(this.canvasToBlob(elem), "file.png");
+    this.qrCode.elementRef.nativeElement.download;
+  }
+
+
+
+
+
+  canvasToBlob(canvas) {
     let dataurl = canvas.toDataURL("image/png");
     let byteString = window.atob(dataurl.replace(/^data:image\/(png|jpg);base64,/, ""));
     const arrayBuffer = new ArrayBuffer(byteString.length);
